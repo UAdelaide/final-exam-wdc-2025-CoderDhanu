18d95479fb4bd49d74a69b5a10af842d51c8fa54
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-06-20 14:54:22 +0000

    [automatic commit] 2025-06-20 14:54:22 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -85,7 +85,6 @@ router.get('/dogs/my-dogs', async (req, res) => {
 
 router.get("/dogs", async (req, res) => {
   console.log("Fetching dogs...");
-  const db = req.app.locals.db;
   try {
     const [rows] = await db.execute(`
       SELECT d.name AS dog_name, d.size, u.username AS owner_username
