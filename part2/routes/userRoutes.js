7b0c6938c90667fb12474bfab0779d0442e5daf4
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-06-20 14:46:31 +0000

    [automatic commit] 2025-06-20 14:46:31 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -83,4 +83,18 @@ router.get('/dogs/my-dogs', async (req, res) => {
   }
 });
 
+router.get("/api/dogs", async (req, res) => {
+  console.log("Fetching dogs...");
+  const db = req.app.locals.db;
+  try {
+    const [rows] = await db.execute(`
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d JOIN Users u ON d.owner_id = u.user_id
+    `);
+    res.json(rows);
+  } catch (err) {
+    res.status(500).json({ error: "Error while fetching dogs" });
+  }
+});
+
 module.exports = router;
