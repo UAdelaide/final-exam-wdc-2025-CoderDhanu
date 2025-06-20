178ff2a5e3090b419691b15c14ded86116f46176
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-06-20 10:50:04 +0000

    [automatic commit] 2025-06-20 10:50:04 UTC

--- part1/routes/users.js
+++ part1/routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", async (req, res) => {
+  console.log("Fetching users...");
+  const db = req.app.locals.db;
+  try {
+    const [rows] = await db.execute(`
+      SELECT username, email, role, created_at FROM Users
+    `);
+    res.json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error fetching users" });
+  }
 });
 
 module.exports = router;
