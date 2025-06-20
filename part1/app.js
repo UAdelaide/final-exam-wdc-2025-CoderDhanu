469d53d8103aee2f4aa34351dd1d77c5c1e9241a
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-06-20 09:41:27 +0000

    [automatic commit] 2025-06-20 09:41:27 UTC

--- part1/app.js
+++ part1/app.js
@@ -10,8 +10,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,7 +35,13 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+  res.json({
+    error: {
+      message: err.message || '',
+      status: err.status || 500
+    }
+  });
 });
 
 module.exports = app;
