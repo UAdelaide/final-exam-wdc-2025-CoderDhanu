ec128d41f7fabc09e8c70debe648fe04f0c441d4
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-06-20 15:11:57 +0000

    [automatic commit] 2025-06-20 15:11:57 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -11,8 +11,12 @@ router.get("/api/dogs", async (req, res) => {
   const db = req.app.locals.db;
   try {
     const [rows] = await db.execute(`
-      SELECT d.name AS dog_name, d.size, u.username AS owner_username
-      FROM Dogs d JOIN Users u ON d.owner_id = u.user_id
+      SELECT
+        d.name AS dog_name,
+        d.size,
+        u.username AS owner_username
+      FROM Dogs d
+      JOIN Users u ON d.owner_id = u.user_id
     `);
     res.json(rows);
   } catch (err) {
