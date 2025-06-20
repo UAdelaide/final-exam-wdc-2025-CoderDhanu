815e0161189db5484bd7e57f45617a15435b805c
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-06-20 11:22:13 +0000

    [automatic commit] 2025-06-20 11:22:13 UTC

--- part1/app.js
+++ part1/app.js
@@ -36,14 +36,14 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
-  // res.status(err.status || 500);
-  // res.render('error');
-  res.json({
-    error: {
-      message: err.message || "manual error",
-      status: err.status || 500,
-    },
-  });
+  res.status(err.status || 500);
+  res.render("error");
+  // res.json({
+  //   error: {
+  //     message: err.message || "manual error",
+  //     status: err.status || 500,
+  //   },
+  // });
 });
 
 // Initialize MySQL and insert test data
