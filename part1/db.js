2496f487532b5bff47b101fd8f80d965334d780c
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-06-20 10:41:13 +0000

    [automatic commit] 2025-06-20 10:41:13 UTC

--- part1/db.js
+++ part1/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "dogpass",
-  database: "DogWalkService",
+  password: "",
+  //   database: "DogWalkService",
 });
 
 module.exports = pool;
