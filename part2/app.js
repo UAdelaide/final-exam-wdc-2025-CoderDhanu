590bba7aae3b3f77cdefa4eca5a3a16abf2724b7
dhanushgajaraj <dhanushsgajaraj@gmail.com>
2025-06-20 15:22:44 +0000

    [automatic commit] 2025-06-20 15:22:44 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,24 +1,26 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const session = require('express-session');
+const session = require("express-session");
 
-app.use(session({
-  secret: 'VmSsaabJOR28eB4UwR8CfbgMZPWqZJXxfpzjEpD6pT4='
-}));
+app.use(
+  session({
+    secret: "VmSsaabJOR28eB4UwR8CfbgMZPWqZJXxfpzjEpD6pT4=",
+  })
+);
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
+const walkRoutes = require("./routes/walkRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/walks", walkRoutes);
+app.use("/api/users", userRoutes);
 
 // Export the app instead of listening here
 module.exports = app;
